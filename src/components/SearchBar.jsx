dfaf4802fc9aef6c4eb890d0ee340b9c6b9b2727
Arman Bhatia <arman.bhatia.1407@gmail.com>
2023-10-27 20:55:11 -0400

    minor fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 const SearchBar = ({ methods }) => {
   return (
-    <form className="max-w-sm m-5">
+    <form className="max-w-sm m-5" onSubmit={(e) => e.preventDefault()}>
       <label
         htmlFor="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only "
