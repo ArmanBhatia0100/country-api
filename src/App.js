f64c3b79d2f93bed31de093cf10e79d89fc64e12
Arman Bhatia <arman.bhatia.1407@gmail.com>
2023-10-29 00:11:03 -0400

    home route created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { Routes, Route } from "react-router";
 
 function App() {
   const [theme, setTheme] = useState("");
@@ -10,12 +11,19 @@ function App() {
   }
 
   return (
-    <div className={`App ${theme}`}>
-      <div className="wrapper">
-        <Header getTheme={getThemeHandler} />
-        <Main />
-      </div>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className={`App ${theme}`}>
+            <div className="wrapper">
+              <Header getTheme={getThemeHandler} />
+              <Main />
+            </div>
+          </div>
+        }
+      ></Route>
+    </Routes>
   );
 }
 
